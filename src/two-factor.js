11aeacfde42736a94252fed40296afeea9ed34c4
FearGannicus <gannicus@exordium.org>
2021-01-12 19:22:41 -0500

    update; organise the string filters

--- src/two-factor.js
+++ src/two-factor.js
@@ -11,8 +11,19 @@ exports.generateSecret = (options, response, error) => {
     }
 
     let bin = crypto.randomBytes(20),
-        base = base32.encode(bin).toString('utf8').replace(/=/g, ''),
-        secret = base.toLowerCase().replace(/(\w{4})/g, "$1 ").trim().split(' ').join('').toUpperCase(),
+        base = base32
+            .encode(bin)
+            .toString('utf8')
+            .replace(/=/g, ''),
+
+        secret = base
+            .toLowerCase()
+            .replace(/(\w{4})/g, "$1 ")
+            .trim()
+            .split(' ')
+            .join('')
+            .toUpperCase(),
+
         uri = `otpauth://totp/${encodeURIComponent(options.app)}:${encodeURIComponent(options.account)}%3Fsecret=${secret}`
 
     return response({
@@ -29,7 +40,10 @@ exports.generateToken = (options, response, error) => {
         return error = 'No secret provided.';
     }
 
-    let unformatted = options.secret.replace(/\W+/g, '').toUpperCase(),
+    let unformatted = options.secret
+            .replace(/\W+/g, '')
+            .toUpperCase(),
+
         bin = base32.decode(unformatted);
 
     return response({
@@ -47,9 +61,14 @@ exports.verifyToken = (options, response, error) => {
         options.window = 4;
     }
 
-    let unformatted = options.secret.replace(/\W+/g, '').toUpperCase(),
+    let unformatted = options.secret
+            .replace(/\W+/g, '')
+            .toUpperCase(),
+
         bin = base32.decode(unformatted),
-        token = options.token.replace(/\W+/g, '');
+        
+        token = options.token
+            .replace(/\W+/g, '');
 
     return response(notp.totp.verify(token, bin, {
         window: options.window,
